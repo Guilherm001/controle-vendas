ea04b70ef28e0b0cfe403d6a43f618d8de320759
Guilherme <guilherm.rodrigues@outlook.com>
2025-06-21 18:03:53 -0300

    retornando ao projeto

--- controle_vendas/src/app/coomponents/barraLateral/BarraLateral.jsx
+++ controle_vendas/src/app/coomponents/barraLateral/BarraLateral.jsx
@@ -5,7 +5,7 @@ import Calculadora from '../calculadora/estrutura/estrutura_Card.jsx';
 export default function BarraLateral() {
     return (
         <div className={Style.container}>
-        <h2>Barra Lateral</h2>
+        <h2>Controle Finanças</h2>
         <ul className={Style.listaLinks}>
             <li><Link href="/">Home</Link></li>
             <li><Link href="/calculadora2">Calculadora</Link></li>
